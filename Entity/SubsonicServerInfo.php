fbdbf003d590f09db82a904814e51d8b73ed9b7a
plfort <plfort@cogipix.com>
2016-04-20 18:56:01 +0200

    Posgtresql migration

--- Entity/SubsonicServerInfo.php
+++ Entity/SubsonicServerInfo.php
@@ -19,7 +19,7 @@ class SubsonicServerInfo
     /**
      * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\GeneratedValue(strategy="AUTO")
      */
     protected $id;
 
